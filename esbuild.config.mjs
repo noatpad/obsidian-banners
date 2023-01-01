10b6751cfb07091d04f3c51ad3651f4d5ec5903e
Lost Paul <70213368+LostPaul@users.noreply.github.com>
2023-01-01 23:23:56 +0100

    Update esbuild.config.mjs

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -11,7 +11,7 @@ Please visit the repository linked to view the source code:
 https://github.com/noatpad/obsidian-banners
 */`;
 const prod = process.argv[2] === 'prod';
-const outdir = prod ? './' : './';
+const outdir = prod ? './' : process.env.DEVDIR;
 
 if (!outdir) {
 	console.error(
