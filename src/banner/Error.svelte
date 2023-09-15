99213c240280a955bb93f601f5c13848daa717d0
Danny Hernandez <tamdannyhdz@gmail.com>
2023-09-15 16:33:13 -0400

    Text align center banner info box

--- src/banner/Error.svelte
+++ src/banner/Error.svelte
@@ -13,19 +13,11 @@
 </div>
 
 <style lang="scss">
+  @import './mixins.scss';
+
   .error {
-    position: absolute;
-    top: 0;
-    left: 0;
-    bottom: 0;
-    right: 0;
-    padding: 16px;
-    border: 4px solid var(--background-modifier-error);
-    margin: 16px;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
+    @include banner-info-box;
+    border-color: var(--background-modifier-error);
     color: var(--text-error);
   }
 </style>
