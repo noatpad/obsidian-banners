57913846f6c58d4d38cd949ee78ae0a6db89be86
Danny Hernandez <tamdannyhdz@gmail.com>
2023-09-19 23:52:49 -0400

    Properly check when to show remove header command

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -76,7 +76,7 @@ const commands: Command[] = [
     name: 'Remove header',
     checkCallback(checking) {
       const file = plug.app.workspace.getActiveFile();
-      if (checking) return !!file && !!extractBannerDataFromFile(file).header !== undefined;
+      if (checking) return !!file && extractBannerDataFromFile(file).header !== undefined;
       updateBannerData(file!, { header: null });
     }
   }
