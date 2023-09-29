ac23d5bc38f439b7d1990a2cd8ac9272ca18df79
Danny Hernandez <tamdannyhdz@gmail.com>
2023-09-29 17:29:18 -0400

    Improve bump script

--- bump.ts
+++ bump.ts
@@ -1,9 +1,10 @@
+/* eslint-disable import/no-named-as-default-member */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { execSync } from 'child_process';
 import { readFile, writeFile } from 'fs/promises';
 import semver from 'semver';
 import type { ReleaseType } from 'semver';
-import { dependencies } from './package.json';
+import { dependencies, version } from './package.json';
 
 const [
   _path,
@@ -18,8 +19,8 @@ const releaseType = `${beta ? 'pre' : ''}${argType}` as ReleaseType;
 const manifestFile = beta ? 'manifest-beta.json' : 'manifest.json';
 const manifest = JSON.parse(await readFile(manifestFile, 'utf-8'));
 const versions = JSON.parse(await readFile('versions.json', 'utf-8'));
-const obsVer = semver.clean(dependencies.obsidian.slice(1))!;
-const newVer = semver.inc(manifest.version, releaseType, 'beta', false)!;
+const obsVer = semver.coerce(dependencies.obsidian)!.version;
+const newVer = semver.inc(semver.coerce(version)!, releaseType, 'beta', false)!;
 
 manifest.version = newVer;
 if (semver.gt(obsVer, manifest.minAppVersion)) {
