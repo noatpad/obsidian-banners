acbbc6ecadc89f4d07cf89727dd2c0470e6c48e1
Danny Hernandez <tamdannyhdz@gmail.com>
2023-09-29 17:15:57 -0400

    Allow bumping beta releases

--- bump.ts
+++ bump.ts
@@ -1,21 +1,32 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { execSync } from 'child_process';
 import { readFile, writeFile } from 'fs/promises';
 import semver from 'semver';
+import type { ReleaseType } from 'semver';
 import { dependencies } from './package.json';
 
-const releaseType: semver.ReleaseType = (process.argv[2] as semver.ReleaseType) ?? 'patch';
+const [
+  _path,
+  _file,
+  argType = 'patch',
+  argBeta
+] = process.argv;
 
-const manifest = JSON.parse(await readFile('manifest.json', 'utf-8'));
+const beta = (argBeta === 'beta');
+const releaseType = `${beta ? 'pre' : ''}${argType}` as ReleaseType;
+
+const manifestFile = beta ? 'manifest-beta.json' : 'manifest.json';
+const manifest = JSON.parse(await readFile(manifestFile, 'utf-8'));
 const versions = JSON.parse(await readFile('versions.json', 'utf-8'));
-const obsVer = semver.clean(dependencies.obsidian.slice(1)) as string;
-const newVer = semver.inc(manifest.version, releaseType) as string;
+const obsVer = semver.clean(dependencies.obsidian.slice(1))!;
+const newVer = semver.inc(manifest.version, releaseType, 'beta', false)!;
 
 manifest.version = newVer;
-if (semver.gt(obsVer, manifest.minAppVersion as string)) {
+if (semver.gt(obsVer, manifest.minAppVersion)) {
   manifest.minAppVersion = obsVer;
   versions[newVer] = obsVer;
 }
 
-await writeFile('manifest.json', JSON.stringify(manifest, null, '\t'));
+await writeFile(manifestFile, JSON.stringify(manifest, null, '\t'));
 await writeFile('versions.json', JSON.stringify(versions, null, '\t'));
-execSync(`npm version ${newVer} -m "Prepare release" --git-tag-version=false`);
+execSync(`npm version ${newVer} --git-tag-version=false`);
